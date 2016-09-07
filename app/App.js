3ec1819abdcf4daf177c73d6232e5b5e914d0f5f
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-09-07 16:05:03 +0700

    add scripts

---
+++ app/App.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import MuiThemeProvider from '../node_modules/material-ui/styles/MuiThemeProvider';
+
--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/App';
-import Welcome from './welcome/Welcome';
-import reducer from './app/reducers/TodoReducer'
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import {Router, Route, Link, browserHistory} from 'react-router'
 
 const store = createStore(reducer, window.devToolsExtension && window.devToolsExtension());
 
@@ -14,10 +11,6 @@ injectTapEventPlugin();
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router history={browserHistory}>
-            <Route path="/" component={Welcome} />
-            <Route path="/todo" component={App} />
-        </Router>
-    </Provider>
-    , document.getElementById('app')
+        <App />
+    </Provider>, document.getElementById('app')
 );
