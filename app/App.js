65f13b3753b8ccfc9e61559a5e2da8c18384d4fa
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-09-07 16:09:24 +0700

    Init component and project structure

--- app/App.js
+++ app/App.js
@@ -1,3 +1,15 @@
 import React from 'react';
 import MuiThemeProvider from '../node_modules/material-ui/styles/MuiThemeProvider';
+import Uploader from './components/Uploader'
 
+class App extends React.Component {
+    render() {
+        return (
+            <MuiThemeProvider >
+                <Uploader />
+            </MuiThemeProvider>
+        );
+    }
+}
+
+export default App
---
+++ app/components/Uploader.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Uploader = () => (
+    <div>
+        <h1>Hello</h1>
+    </div>
+);
+
+export default Uploader
