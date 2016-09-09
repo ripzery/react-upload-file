4921bb5585221f8a97c9a1cb353da24dc3809a63
Euro <euro@socket9.com>
2016-09-09 16:37:27 +0700

    Add isomorphic-fetch

---
+++ app/components/UploadButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FlatButton from 'material-ui/FlatButton';
+import 'whatwg-fetch'
+
+class UploadButton extends React.Component {
+    render(){
+        return (
+            
+        )
+    }
+}
+
+export default UploadButton
