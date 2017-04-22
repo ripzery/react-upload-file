c0ff4460357ce95bdd3f343c4b968fcc7a39b625
Ripzery <ripzery@gmail.com>
2017-04-23 02:12:28 +0700

    Change App Title

--- app/components/Appbar.js
+++ app/components/Appbar.js
@@ -17,7 +17,7 @@ class Appbar extends React.Component {
         this.upload = this.upload.bind(this);
         this.state = {
             count: 0,
-            title: "Gallery Cutes",
+            title: "Gallery Cutes Na",
             uploading: false,
             selectAlbum: 0
         };
