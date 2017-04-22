754c298dbebd81d91c48fb39fab1c85eb50aa460
Ripzery <ripzery@gmail.com>
2017-04-23 02:00:58 +0700

    Changle toolbar title

--- app/components/Appbar.js
+++ app/components/Appbar.js
@@ -17,7 +17,7 @@ class Appbar extends React.Component {
         this.upload = this.upload.bind(this);
         this.state = {
             count: 0,
-            title: "Gallery Cute",
+            title: "Gallery Cutes",
             uploading: false,
             selectAlbum: 0
         };
