9ccafda962535eba87e023663108f29551bcdaf2
Ripzery <ripzery@gmail.com>
2017-04-23 18:53:37 +0700

    Revert name

--- app/components/Appbar.js
+++ app/components/Appbar.js
@@ -17,7 +17,7 @@ class Appbar extends React.Component {
         this.upload = this.upload.bind(this);
         this.state = {
             count: 0,
-            title: "Gallery MVP 55555555",
+            title: "Gallery MVP",
             uploading: false,
             selectAlbum: 0
         };
